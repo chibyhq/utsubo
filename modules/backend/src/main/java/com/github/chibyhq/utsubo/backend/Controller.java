d733eb11fe93967b3804fc052b7325fbcd93f607
bcopy <500789+bcopy@users.noreply.github.com>
2020-04-26 18:30:47 +0200

    Add backend skeleton

---
+++ modules/backend/src/main/java/com/github/chibyhq/utsubo/backend/Controller.java
@@ -0,0 +1,15 @@
+package com.github.chibyhq.utsubo.backend;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    
+    @GetMapping("/")
+    public String foo() {
+        return "Hello from tomcat";
+    }
+    
+}
