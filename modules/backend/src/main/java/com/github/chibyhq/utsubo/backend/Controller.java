1b93a672eae3550cce88db0f18cddd969a889f10
bcopy <500789+bcopy@users.noreply.github.com>
2020-04-27 19:46:04 +0200

    Add static resources

--- modules/backend/src/main/java/com/github/chibyhq/utsubo/backend/Controller.java
+++ modules/backend/src/main/java/com/github/chibyhq/utsubo/backend/Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
     
-    @GetMapping("/")
+    @GetMapping("/greeting")
     public String foo() {
         return "Hello from tomcat";
     }
